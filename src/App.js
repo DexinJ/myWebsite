f62877c8a6c842e390f0d63a673d7c57bfa0e7ec
DexinJ <jindexin6@gmail.com>
2023-08-26 23:19:08 -0700

    restored fileread

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
-import Rage from './pages/rage';
-import AIproject from './pages/aiproject';
-import WebProject from './pages/webProject';
-import FSileread from './pages/fileread';
-import Gridtest from './pages/grid';
-import Home from './pages';
+import Rage from "./pages/rage";
+import AIproject from "./pages/aiproject";
+import WebProject from "./pages/webProject";
+import FSileread from "./pages/fileread";
+import Gridtest from "./pages/grid";
+import Home from "./pages";
 
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/rage' element={<Rage/>} />
-            <Route path='/AI' element={<AIproject/>} />
-            <Route path='/web' element={<WebProject/>} />
-            {/* <Route path='/fileread' element={<FSileread/>} /> */}
-          </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/rage" element={<Rage />} />
+        <Route path="/AI" element={<AIproject />} />
+        <Route path="/web" element={<WebProject />} />
+        <Route path="/fileread" element={<FSileread />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
