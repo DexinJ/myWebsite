b01b8ef650a823c206c85118c09e67b5ba3f30aa
DexinJ <dejin@ucsc.edu>
2024-09-25 12:41:09 -0700

    new version

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Header.module.css";
+import Logo from "../Logo/Logo";
+import Navigation from "../Navigation/Navigation";
+import ThemeToggle from "../ThemeToggle/THemetoggle";
+import Button from "../Button/Button";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <Navigation />
+      <div className={styles.rightContent}>
+        <ThemeToggle />
+        <Button className={styles.resumeButton}>Resume</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
